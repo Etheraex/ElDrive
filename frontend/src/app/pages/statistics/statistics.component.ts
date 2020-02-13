69efae8bb7e6088a7d8c7b46a0fb337edf8e20e2
mihajlo <mihajlo@boemskats.com>
2020-02-13 16:55:54 +0100

    WIP chart

--- frontend/src/app/pages/statistics/statistics.component.ts
+++ frontend/src/app/pages/statistics/statistics.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StatisticsService } from 'src/app/services/statistics.service';
 import { Statistics } from 'src/app/models/statistics.model';
+import * as Highcharts  from 'highcharts';
 
 @Component({
   selector: 'app-statistics',
@@ -12,14 +13,77 @@ export class StatisticsComponent implements OnInit {
 	data : Statistics;
 	constructor(private statisticsService : StatisticsService) {
 		this.data = new Statistics();
-	 }
-
+	}
+	
 	ngOnInit() {
-	this.statisticsService.getStatistics()
-	.subscribe((result : Statistics) => {
-		this.data = result;
-		console.log(this.data);
-	})
+		this.statisticsService.getStatistics()
+		.subscribe((result : Statistics) => {
+			this.data = result;
+		})
+		this.drawchart();
+	}
+	
+	drawchart(){
+		Highcharts.chart( {
+			chart:{
+
+				renderTo:'container',
+				
+			},
+			title: {
+				text: 'Solar Employment Growth by Sector, 2010-2016'
+			},
+			
+			series:[{type:"line",name:"test",data:[43,22]}],
+
+			subtitle: {
+				text: 'Source: thesolarfoundation.com'
+			},
+		
+			yAxis: {
+				title: {
+					text: 'Number of Employees'
+				}
+			},
+			
+			xAxis: {
+				accessibility: {
+					rangeDescription: 'Range: 2010 to 2017'
+				}
+			},
+			
+			legend: {
+				layout: 'vertical',
+				align: 'right',
+				verticalAlign: 'middle'
+			},
+			
+			plotOptions: {
+				series: {
+					label: {
+						connectorAllowed: false
+					},
+					pointStart: 2010
+				}
+			},
+			
+			
+			responsive: {
+				rules: [{
+					condition: {
+						maxWidth: 500
+					},
+					chartOptions: {
+						legend: {
+							layout: 'horizontal',
+							align: 'center',
+							verticalAlign: 'bottom'
+						}
+					}
+				}]
+			}
+			
+		});
 	}
 
 }
