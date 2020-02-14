a60d9cd8e1863064675f11f8bc13e9dac97a3c01
Stefan <stefan.bogosavljevic@elfak.rs>
2020-02-14 09:36:21 +0100

    fix Typo: changed contant to content

--- note-service/Models/Note.cs
+++ note-service/Models/Note.cs
@@ -5,7 +5,7 @@ namespace note_service.Models
     public class Note
     {
         public string Title { get; set; }
-		public string Contant { get; set; }
+		public string Content { get; set; }
 		public DateTime CreationDate { get; set; }
 		public DateTime LastEdited { get; set; }
     }
