693744c082314e366de36ad51dca3d36b456a915
Etheraex <mladjaether@gmail.com>
2020-04-01 11:59:51 +0200

    Load connection string from file

--- mongo-config/Config/MongoDbConfig.cs
+++ mongo-config/Config/MongoDbConfig.cs
@@ -1,8 +1,18 @@
+using System.IO;
+
 namespace mongo_config
 {
 	public class MongoDBConfig
 	{
 		public string Database => "ZI-db";
-		public string ConnectionString => "Connection-string";
+		public string ConnectionString => LoadConnectionString();
+
+		private string LoadConnectionString()
+		{
+			using (StreamReader sr = new StreamReader("../../zi/environment.txt"))
+			{
+				return sr.ReadLine();
+			}
+		}
 	}
 }
